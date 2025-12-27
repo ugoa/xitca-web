063186dcca58872a59cdc80243f06d3fb1d04825
Dawei Hu <hoodavy@gmail.com>
2025-12-27 19:14:37 +0800

    ad

--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -1,10 +1,14 @@
 //! A Http server returns Hello World String as Response.
 
-use xitca_web::{handler::handler_service, route::get, App};
+use xitca_web::{App, handler::handler_service, route::get};
+
+async fn hi() -> &'static str {
+    "Hello world"
+}
 
 fn main() -> std::io::Result<()> {
     App::new()
-        .at("/", get(handler_service(async || "Hello,World!" )))
+        .at("/", get(handler_service(hi)))
         .serve()
         .bind("127.0.0.1:8080")?
         .run()
